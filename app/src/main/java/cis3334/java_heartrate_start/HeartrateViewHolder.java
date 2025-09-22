5fc64839dcd5deddb64a6d75a9583dfe2c7b9f36
Takunda Madziwa <tvmadziwa5@gmail.com>
2025-09-21 20:37:17 -0500

    add heart viewholder and layout files

---
+++ app/src/main/java/cis3334/java_heartrate_start/HeartrateViewHolder.java
@@ -0,0 +1,26 @@
+package cis3334.java_heartrate_start;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class HeartrateViewHolder extends RecyclerView.ViewHolder {
+
+    TextView textViewPulse;
+    TextView textViewRange;
+
+    TextView textViewDescription;
+
+    public HeartrateViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        textViewPulse = itemView.findViewById(R.id.textPulse);
+        textViewRange = itemView.findViewById(R.id.textRange);
+        textViewDescription = itemView.findViewById(R.id.description);
+
+    }
+}
+
+
